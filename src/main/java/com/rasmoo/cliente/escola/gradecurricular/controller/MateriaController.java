9dacfdcd0366e449d3e13219f87b57abd1d28d5b
Vinicius Bastos <vinicius_hora@live.com>
2021-04-01 18:40:56 -0300

    criação da primeira controler

---
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/controller/MateriaController.java
@@ -0,0 +1,19 @@
+package com.rasmoo.cliente.escola.gradecurricular.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/materia")
+public class MateriaController {
+	
+	@GetMapping("/")
+	public ResponseEntity<String> helloWordRest(){
+		return ResponseEntity.status(HttpStatus.OK).body("Olá mundo REST");
+	}
+	
+
+}
